54bf733c9710615e8d711a55f97cacfaeacafc3f
MeisterYoda233 <MeisterYoda233@users.noreply.gitgud.io>
2024-04-27 23:59:39 +0200

    GUI added

--- Taschenrechner/Form1.Designer.cs
+++ Taschenrechner/Form1.Designer.cs
@@ -28,12 +28,258 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btn_7 = new Button();
+            btn_8 = new Button();
+            btn_9 = new Button();
+            btn_6 = new Button();
+            btn_5 = new Button();
+            btn_4 = new Button();
+            btn_3 = new Button();
+            btn_2 = new Button();
+            btn_1 = new Button();
+            btn_mul = new Button();
+            btn_sub = new Button();
+            btn_add = new Button();
+            btn_div = new Button();
+            tbx_calculation = new TextBox();
+            btn_sqrt = new Button();
+            btn_quadrat = new Button();
+            btn_calc = new Button();
+            btn_del = new Button();
+            btn_AC = new Button();
+            btn_hochX = new Button();
+            SuspendLayout();
+            // 
+            // btn_7
+            // 
+            btn_7.Location = new Point(42, 118);
+            btn_7.Name = "btn_7";
+            btn_7.Size = new Size(75, 23);
+            btn_7.TabIndex = 0;
+            btn_7.Text = "7";
+            btn_7.UseVisualStyleBackColor = true;
+            // 
+            // btn_8
+            // 
+            btn_8.Location = new Point(123, 118);
+            btn_8.Name = "btn_8";
+            btn_8.Size = new Size(75, 23);
+            btn_8.TabIndex = 1;
+            btn_8.Text = "8";
+            btn_8.UseVisualStyleBackColor = true;
+            // 
+            // btn_9
+            // 
+            btn_9.Location = new Point(204, 118);
+            btn_9.Name = "btn_9";
+            btn_9.Size = new Size(75, 23);
+            btn_9.TabIndex = 2;
+            btn_9.Text = "9";
+            btn_9.UseVisualStyleBackColor = true;
+            // 
+            // btn_6
+            // 
+            btn_6.Location = new Point(204, 147);
+            btn_6.Name = "btn_6";
+            btn_6.Size = new Size(75, 23);
+            btn_6.TabIndex = 5;
+            btn_6.Text = "6";
+            btn_6.UseVisualStyleBackColor = true;
+            // 
+            // btn_5
+            // 
+            btn_5.Location = new Point(123, 147);
+            btn_5.Name = "btn_5";
+            btn_5.Size = new Size(75, 23);
+            btn_5.TabIndex = 4;
+            btn_5.Text = "5";
+            btn_5.UseVisualStyleBackColor = true;
+            // 
+            // btn_4
+            // 
+            btn_4.Location = new Point(42, 147);
+            btn_4.Name = "btn_4";
+            btn_4.Size = new Size(75, 23);
+            btn_4.TabIndex = 3;
+            btn_4.Text = "4";
+            btn_4.UseVisualStyleBackColor = true;
+            // 
+            // btn_3
+            // 
+            btn_3.Location = new Point(204, 176);
+            btn_3.Name = "btn_3";
+            btn_3.Size = new Size(75, 23);
+            btn_3.TabIndex = 8;
+            btn_3.Text = "3";
+            btn_3.UseVisualStyleBackColor = true;
+            // 
+            // btn_2
+            // 
+            btn_2.Location = new Point(123, 176);
+            btn_2.Name = "btn_2";
+            btn_2.Size = new Size(75, 23);
+            btn_2.TabIndex = 7;
+            btn_2.Text = "2";
+            btn_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_1
+            // 
+            btn_1.Location = new Point(42, 176);
+            btn_1.Name = "btn_1";
+            btn_1.Size = new Size(75, 23);
+            btn_1.TabIndex = 6;
+            btn_1.Text = "1";
+            btn_1.UseVisualStyleBackColor = true;
+            // 
+            // btn_mul
+            // 
+            btn_mul.Location = new Point(306, 176);
+            btn_mul.Name = "btn_mul";
+            btn_mul.Size = new Size(75, 23);
+            btn_mul.TabIndex = 11;
+            btn_mul.Text = "*";
+            btn_mul.UseVisualStyleBackColor = true;
+            // 
+            // btn_sub
+            // 
+            btn_sub.Location = new Point(306, 147);
+            btn_sub.Name = "btn_sub";
+            btn_sub.Size = new Size(75, 23);
+            btn_sub.TabIndex = 10;
+            btn_sub.Text = "-";
+            btn_sub.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            btn_add.Location = new Point(306, 118);
+            btn_add.Name = "btn_add";
+            btn_add.Size = new Size(75, 23);
+            btn_add.TabIndex = 9;
+            btn_add.Text = "+";
+            btn_add.UseVisualStyleBackColor = true;
+            // 
+            // btn_div
+            // 
+            btn_div.Location = new Point(306, 205);
+            btn_div.Name = "btn_div";
+            btn_div.Size = new Size(75, 23);
+            btn_div.TabIndex = 12;
+            btn_div.Text = "/";
+            btn_div.UseVisualStyleBackColor = true;
+            // 
+            // tbx_calculation
+            // 
+            tbx_calculation.Location = new Point(42, 89);
+            tbx_calculation.Name = "tbx_calculation";
+            tbx_calculation.Size = new Size(339, 23);
+            tbx_calculation.TabIndex = 13;
+            // 
+            // btn_sqrt
+            // 
+            btn_sqrt.Location = new Point(204, 205);
+            btn_sqrt.Name = "btn_sqrt";
+            btn_sqrt.Size = new Size(75, 23);
+            btn_sqrt.TabIndex = 14;
+            btn_sqrt.Text = "√";
+            btn_sqrt.UseVisualStyleBackColor = true;
+            // 
+            // btn_quadrat
+            // 
+            btn_quadrat.Location = new Point(123, 205);
+            btn_quadrat.Name = "btn_quadrat";
+            btn_quadrat.Size = new Size(75, 23);
+            btn_quadrat.TabIndex = 15;
+            btn_quadrat.Text = "X²";
+            btn_quadrat.UseVisualStyleBackColor = true;
+            // 
+            // btn_calc
+            // 
+            btn_calc.Location = new Point(42, 234);
+            btn_calc.Name = "btn_calc";
+            btn_calc.Size = new Size(75, 23);
+            btn_calc.TabIndex = 16;
+            btn_calc.Text = "=";
+            btn_calc.UseVisualStyleBackColor = true;
+            // 
+            // btn_del
+            // 
+            btn_del.Location = new Point(123, 234);
+            btn_del.Name = "btn_del";
+            btn_del.Size = new Size(75, 23);
+            btn_del.TabIndex = 17;
+            btn_del.Text = "DEL";
+            btn_del.UseVisualStyleBackColor = true;
+            // 
+            // btn_AC
+            // 
+            btn_AC.Location = new Point(204, 234);
+            btn_AC.Name = "btn_AC";
+            btn_AC.Size = new Size(75, 23);
+            btn_AC.TabIndex = 18;
+            btn_AC.Text = "AC";
+            btn_AC.UseVisualStyleBackColor = true;
+            // 
+            // btn_hochX
+            // 
+            btn_hochX.Location = new Point(42, 205);
+            btn_hochX.Name = "btn_hochX";
+            btn_hochX.Size = new Size(75, 23);
+            btn_hochX.TabIndex = 19;
+            btn_hochX.Text = "xˣ";
+            btn_hochX.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(427, 450);
+            Controls.Add(btn_hochX);
+            Controls.Add(btn_AC);
+            Controls.Add(btn_del);
+            Controls.Add(btn_calc);
+            Controls.Add(btn_quadrat);
+            Controls.Add(btn_sqrt);
+            Controls.Add(tbx_calculation);
+            Controls.Add(btn_div);
+            Controls.Add(btn_mul);
+            Controls.Add(btn_sub);
+            Controls.Add(btn_add);
+            Controls.Add(btn_3);
+            Controls.Add(btn_2);
+            Controls.Add(btn_1);
+            Controls.Add(btn_6);
+            Controls.Add(btn_5);
+            Controls.Add(btn_4);
+            Controls.Add(btn_9);
+            Controls.Add(btn_8);
+            Controls.Add(btn_7);
+            Name = "Form1";
+            Text = "Taschenrechner";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btn_7;
+        private Button btn_8;
+        private Button btn_9;
+        private Button btn_6;
+        private Button btn_5;
+        private Button btn_4;
+        private Button btn_3;
+        private Button btn_2;
+        private Button btn_1;
+        private Button btn_mul;
+        private Button btn_sub;
+        private Button btn_add;
+        private Button btn_div;
+        private TextBox tbx_calculation;
+        private Button btn_sqrt;
+        private Button btn_quadrat;
+        private Button btn_calc;
+        private Button btn_del;
+        private Button btn_AC;
+        private Button btn_hochX;
     }
 }
